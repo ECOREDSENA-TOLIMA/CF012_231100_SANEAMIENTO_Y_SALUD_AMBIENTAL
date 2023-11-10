53c94c53f373053b9ae36dac9124beacb88ccf0a
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-11-10 10:57:38 -0500

    Update temas 4-5

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -6,7 +6,99 @@
       .titulo-principal__numero
         span 4
       h1 Monitoreo de ruido
+    .row.justify-content-center.align-items-center 
+      .col-lg-8.col-12.mb-3
+        p(data-aos="fade-left") El  monitoreo del ruido es una función clave de las autoridades de control ambiental para identificar y controlar la contaminación auditiva. Esto implica la identificación de fuentes de ruido, ya sean fijas o móviles, y la medición de sus niveles de ruido, así como la evaluación de su naturaleza y frecuencia. Las Corporaciones Autónomas Regionales y las autoridades ambientales en centros urbanos tienen la responsabilidad de llevar a cabo este seguimiento para evaluar y reducir el ruido en sus áreas. Este monitoreo se enfoca en tres aspectos principales:
+        .tarjeta.p-3.mb-3(style="background-color: #DEFFF8") #[b a) Monitoreo de fuentes fijas:] identifica estas fuentes, describe su naturaleza, frecuencia y momentos de mayor intensidad de ruido, y evalúa las medidas tomadas para mitigar los efectos del ruido. Se verifica si cumplen con los niveles máximos permitidos según la normativa aplicable.
+        .tarjeta.p-3.mb-3(style="background-color: #DEFFF8") #[b b) Monitoreo de fuentes móviles:] se realizan pruebas estáticas y dinámicas para determinar los niveles de ruido emitidos por vehículos y equipos móviles durante su operación.
+        .tarjeta.p-3.mb-3(style="background-color: #DEFFF8") #[b c) Monitoreo de ruido ambiental:] se enfoca en evaluar el comportamiento del ruido en áreas específicas, especialmente en zonas con más de 100,000 habitantes. Se utiliza para crear mapas de ruido que ayudan en la planificación urbana y en el desarrollo de estrategias para mitigar los impactos del ruido en estas áreas.
+      .col-lg-4.col-md-6.col-12.mb-3
+        figure(data-aos="fade-right") 
+          img(src='@/assets/curso/temas/tema4/1.png', alt='Imagen decorativa')
+    .row
+      p(data-aos="fade-left") La Resolución 0627 del 2006 en Colombia establece normas específicas para la medición y monitoreo de niveles de ruido. En cuanto a los equipos de medición, la norma proporciona recomendaciones precisas:
+    .row.justify-content-center.align-items-center 
+      .col-lg-4.col-md-6.col-12.mb-3
+        figure(data-aos="fade-right") 
+          img(src='@/assets/curso/temas/tema4/2.svg', alt='Imagen decorativa')
+      .col-lg-8.col-12.mb-3
+        .tarjeta.p-3.mb-3(style="background-color: #EEFFDE")
+          ul.lista-ul
+            li 
+              i.fas.fa-check(style="color: #63C500")
+              | #[b Tipo de instrumentos:] se requieren sonómetros de tipo 1 o, como mínimo, tipo 2. Estos sonómetros deben ser capaces de medir el equivalente de presión sonora con ponderación frecuencial (A)  directa o indirecta (dB(A)). La ponderación (A) filtra las altas y bajas frecuencias, lo que coincide con la sensibilidad del oído humano a las frecuencias sonoras.
+        .tarjeta.p-3.mb-3(style="background-color: #EEFFDE")
+          ul.lista-ul
+            li 
+              i.fas.fa-check(style="color: #63C500")
+              | #[b Dotación de los equipos:] los sonómetros deben estar equipados con un pistófono o calibrador para asegurar la precisión, una pantalla antiviento para evitar distorsiones debido al viento y un trípode para el montaje. Para mediciones de ruido ambiental, se recomienda una extensión de micrófono.
+          
+        .tarjeta.p-3.mb-3(style="background-color: #EEFFDE")
+          ul.lista-ul
+            li 
+              i.fas.fa-check(style="color: #63C500")
+              | #[b Calibraciones:] antes de realizar mediciones, los equipos deben calibrarse según las condiciones del entorno. Se sugiere utilizar un pistófono o calibrador para este propósito. Los certificados de calibración electrónica deben estar actualizados y adjuntarse al informe técnico.
+        .tarjeta.p-3.mb-3(style="background-color: #EEFFDE")
+          ul.lista-ul
+            li 
+              i.fas.fa-check(style="color: #63C500")
+              | #[b Condiciones meteorológicas:] las mediciones deben llevarse a cabo en condiciones de tiempo seco, sin lluvia, llovizna, truenos o granizo. En caso de mediciones en carreteras, los pavimentos deben estar secos, y la velocidad del viento no debe superar los tres metros por segundo (3 m/s).
+    .row
+      .cajon.p-4(style="background-color: #DEFFF8")
+        p(data-aos="fade-left") Estas directrices aseguran que las mediciones de ruido se realicen de manera precisa y estandarizada, lo que es esencial para evaluar y controlar los niveles de ruido de acuerdo con los requisitos legales y regulatorios.
+    .row.mt-3
+      p(data-aos="fade-left") Los equipos de medición de ruido desempeñan un papel fundamental en la sociedad al permitir el cumplimiento de normativas, la protección de la salud pública, el control de la contaminación acústica y la planificación de entornos urbanos adecuados. Además, facilitan investigaciones sobre los impactos del ruido en la salud, la monitorización continua de áreas críticas, el desarrollo de estrategias de mitigación y la sensibilización del público sobre la importancia de reducir la contaminación acústica, contribuyendo a entornos más saludables y sostenibles. A continuación, se exponen algunos de los equipos utilizados en la medición, así como sus características y/o recomendaciones de uso.
+
+    .row.mt-3 
+      .tarjeta.tarjeta.p-4.mb-5(style="background-color: #EEFFDE")
+        SlyderA(tipo="b")
+          .row
+            .col-md-6.mb-4.mb-md-0
+              h4 Sonómetros tipo 1
+              p Son dispositivos altamente precisos utilizados para medir niveles de ruido con exactitud. Son ideales para aplicaciones técnicas y estudios que requieren mediciones muy precisas, como evaluaciones en entornos industriales y estudios de impacto ambiental. Estos sonómetros cumplen con estándares rigurosos y son esenciales para garantizar el cumplimiento de regulaciones y normativas relacionadas con el control del ruido.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/3.jpg', alt='Imagen decorativa')
+          .row
+            .col-md-6.mb-4.mb-md-0
+              h4 Sonómetros tipo 2
+              p Son instrumentos de medición de niveles de ruido menos precisos que los sonómetros tipo 1, pero aún adecuados para muchas aplicaciones. Son ampliamente utilizados en situaciones generales de medición de ruido, como evaluaciones del ruido ambiental, verificaciones de cumplimiento y aplicaciones de control de ruido en áreas urbanas. Aunque ofrecen una precisión moderada, son suficientes para la mayoría de los propósitos técnicos y reguladores relacionados con el ruido.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/4.jpg', alt='Imagen decorativa')
+          .row
+            .col-md-6.mb-4.mb-md-0
+              h4 Sonómetro digital tipo 1 con trípode
+              p Es un instrumento altamente preciso y versátil utilizado para medir niveles de ruido en diversas aplicaciones, desde estudios técnicos detallados hasta evaluaciones de ruido ambiental. Su alta precisión lo hace adecuado para situaciones donde se requiere un alto grado de exactitud en las mediciones. Además, el trípode proporciona estabilidad al equipo, lo que mejora aún más la precisión de las mediciones. Estos instrumentos son esenciales en el monitoreo y control de la contaminación auditiva y son ampliamente utilizados en campos como la acústica, la ingeniería de sonido y la evaluación ambiental.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/5.jpg', alt='Imagen decorativa')
+          .row
+            .col-md-6.mb-4.mb-md-0
+              h4 Sonómetro y calibrador acústico
+              p Son herramientas críticas en la medición precisa de niveles de ruido. El sonómetro es un dispositivo utilizado para registrar y cuantificar los niveles de presión sonora en el entorno, mientras que el calibrador acústico es una herramienta que se utiliza para ajustar y verificar la precisión del sonómetro. La calibración es esencial para garantizar que el sonómetro ofrezca mediciones precisas y confiables. Estas herramientas se utilizan en una variedad de aplicaciones, como la evaluación de la contaminación auditiva, el monitoreo de ruido ambiental y la conformidad con regulaciones de seguridad ocupacional. La inclusión de información sobre la calibración en los informes técnicos es fundamental para respaldar la calidad de las mediciones y garantizar su validez.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/6.jpg', alt='Imagen decorativa')
+          .row
+            .col-md-6.mb-4.mb-md-0
+              h4 Kit de sonómetro, calibrador y trípode
+              p Un kit que incluye un sonómetro, un calibrador y un trípode es una combinación ideal para realizar mediciones de ruido precisas y confiables. El sonómetro se utiliza para registrar los niveles de ruido, el calibrador asegura que el sonómetro esté calibrado correctamente para mediciones precisas, y el trípode proporciona estabilidad al equipo durante las mediciones, lo que es esencial para obtener resultados precisos y coherentes. Este kit es ampliamente utilizado en estudios técnicos y aplicaciones donde la precisión es fundamental.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/7.jpg', alt='Imagen decorativa')
     
+    .row.justify-content-center
+      .col-lg-8.col-12 
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 3.
+          span #[i Partes equipos de medición (Sonómetro)]
+        figure.desktop(data-aos="fade-right")
+          img(src='@/assets/curso/temas/tema4/8.svg' alt='En la figura 3 se muestra un sonómetro y cada una de las partes que lo componen.')
+    .row.justify-content-center
+      .col-lg-6.col-12 
+        figure.movil(data-aos="fade-right")
+          img(src='@/assets/curso/temas/tema4/9.svg' alt='En la figura 3 se muestra un sonómetro y cada una de las partes que lo componen.')
 
 </template>
 
--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -6,12 +6,152 @@
       .titulo-principal__numero
         span 5
       h1 Presentación de informes
+    .row.justify-content-center.align-items-center 
+      .col-lg-8.col-12.mb-3
+        .cajon.p-3(style="background-color: #E7FFD1")
+          p(data-aos="fade-left") La presentación de informes técnicos es un paso crucial en los procesos de evaluación de parámetros ambientales como el monitoreo de olores ofensivos y ruido. Estos informes son fundamentales para la toma de decisiones, la formulación de proyectos y la planificación urbana y rural. Un informe técnico es un documento que comunica información recopilada y analizada de manera rigurosa, basándose en criterios específicos. La calidad de los datos recopilados y la precisión en el análisis son esenciales para garantizar la fiabilidad de estos informes.
+      .col-lg-4.col-md-8.col-12.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema5/1.png', alt='Imagen decorativa')
     separador 
     .titulo-segundo.color-acento-contenido
       h2#t_5_1 5.1 Informe de monitoreo de olores ofensivos
+    .row.justify-content-center.align-items-center 
+      .col-lg-4.col-md-8.col-12.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema5/2.png', alt='Imagen decorativa')
+      .col-lg-8.col-12.mb-3
+        p(data-aos="fade-left") En la elaboración de un informe sobre olores ofensivos, conforme a las directrices de la Resolución 2087 de 2014, es esencial incorporar una serie de elementos fundamentales. Estos comprenden los siguientes aspectos:
+        p(data-aos="fade-left") #[b a) Datos generales:]  esta sección proporciona una visión general de la zona de estudio e incluye detalles relevantes para analizar las interacciones que pueden influir en el problema identificado. Entre estos datos generales, se deben considerar:
+        .cajon.p-4(style="background-color: #EEFAFF")
+          .row.justify-content-center 
+            .col-lg-10.col-12
+              ul.lista-ul
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Localización:] se debe especificar la ubicación precisa de la problemática, su distancia con respecto a centros poblados, vías de acceso, características geográficas, áreas de interés ecológico, población circundante y factores ambientales y climáticos predominantes.
+              ul.lista-ul
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Identificación de la problemática:] aquí se describe en términos generales la naturaleza de la problemática y cómo se ve influenciada por factores tanto naturales como antropogénicos.
+              ul.lista-ul
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Descripción de la actividad:] se detalla la actividad productiva relacionada con la problemática, incluyendo su duración, frecuencia y condiciones bajo las cuales se realiza. También se debe mencionar si estas actividades están de acuerdo con las regulaciones del ordenamiento territorial.
+    
+    .row.justify-content-center.align-items-center 
+      .col-lg-8.col-12.mb-3
+        .tarjeta.p-4.mb-2(style="background-color: #EEFFDE") #[b b) Métodos de muestreo y análisis utilizados:] en esta sección se especifica la metodología empleada para recopilar información, cómo se determinó el tamaño de la muestra, la selección de los sitios de muestreo, la cantidad de muestras recogidas y los equipos utilizados. La claridad y detalle en esta área contribuyen a una comprensión precisa del estudio.
+        .tarjeta.p-4.mb-2(style="background-color: #EEFFDE") #[b c) Localización del sitio de monitoreo:] esta parte del informe describe de manera minuciosa el lugar específico donde se llevaron a cabo las mediciones. Debe incluir detalles sobre la altitud, velocidad del viento, temperatura y hora en la que se recolectaron las muestras, entre otros aspectos relevantes.
+        .tarjeta.p-4.mb-2(style="background-color: #EEFFDE") #[b d) Modelos de dispersión de contaminantes:] si se utilizaron modelos de dispersión o se generaron mapas, se deben incluir todos los detalles que faciliten su interpretación. Esto abarca la información esencial para el modelo, como condiciones meteorológicas y topográficas, los criterios de modelación empleados y los resultados de las concentraciones máximas en intervalos horarios y diarios de las sustancias contaminantes durante el período evaluado.
+      .col-lg-4.col-md-8.col-12.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema5/3.svg', alt='Imagen decorativa')
+
+    .row.justify-content-center.align-items-center 
+      .col-lg-4.col-md-8.col-12.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema5/4.svg', alt='Imagen decorativa')
+      .col-lg-8.col-12.mb-3
+        .tarjeta.p-4.mb-2(style="background-color: #EEFFDE") #[b e) Análisis de resultados y causas:] es crucial realizar un análisis de los resultados obtenidos y de las causas subyacentes de la problemática. Este análisis debe ser riguroso y basarse en observaciones y pruebas técnicas.
+        .tarjeta.p-4.mb-2(style="background-color: #EEFFDE") #[b f) Información secundaria:] esta sección refuerza los aspectos analizados en el informe, proporcionando datos adicionales que contribuyan a la comprensión de los fenómenos observados.
+        .tarjeta.p-4.mb-2(style="background-color: #EEFFDE") #[b g) Conclusiones:] deben estar respaldadas por la información recopilada y resumir de manera concisa los aspectos más relevantes descubiertos durante el proceso de observación.
+        .tarjeta.p-4.mb-2(style="background-color: #EEFFDE") #[b h) Recomendaciones:] se incluyen recomendaciones dirigidas a las autoridades locales para identificar soluciones alternativas y medidas de mitigación con el objetivo de abordar la problemática de los olores ofensivos de manera efectiva. Estas recomendaciones deben ser prácticas y basadas en la evidencia recopilada.
+    .row 
+      p(data-aos="fade-left") Esta estructura garantiza que el informe sea completo, informativo y útil para la toma de decisiones y la resolución de problemas relacionados con los olores ofensivos.
+      
     separador 
     .titulo-segundo.color-acento-contenido
       h2#t_5_2 5.2 Informe de monitoreo de ruido
+    p(data-aos="fade-left") Un informe de monitoreo de ruido, como establece la Resolución 0627 de 2006, debe incluir una serie de elementos clave para reflejar de manera precisa los resultados del trabajo de campo. Los aspectos que se deben considerar en la presentación de dicho informe incluyen:
+    .row.justify-content-center.align-items-center 
+      .col-lg-8.col-12.mb-3
+        .tarjeta.p-1(style="background-color: #EEFAFF")
+          .row.justify-content-center
+            .col-lg-10.col-12
+              ul.lista-ul
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Fecha y hora de la medición:] debe indicarse cuándo se realizó el monitoreo y la duración del mismo.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Responsable del informe:] se debe proporcionar información básica sobre la persona o entidad que generó el informe.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Ubicación de la medición:] se especifica el lugar donde se realizaron las mediciones de ruido.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Propósito de la medición:] se describe el motivo por el cual se llevó a cabo el monitoreo de ruido.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Norma utilizada:] si se aplicó una norma específica para la medición, se debe indicar la norma y las razones para su elección.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Tipo de instrumentación utilizado:] debe especificarse qué tipo de instrumentos se emplearon para la medición.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Equipo de medición utilizado:] incluye información sobre los números de serie de los instrumentos utilizados.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Datos de calibración:] se indican los datos de calibración, el ajuste del instrumento y la fecha de vencimiento del certificado de calibración del pistófono.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Procedimiento de medición:] se describe cómo se realizaron las mediciones de ruido.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Ruido residual:] si no se pudo medir el ruido residual, se deben proporcionar las razones detrás de esta limitación.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b Condiciones predominantes generales:] se detallan las condiciones predominantes en la zona de estudio, como características geográficas y ambientales.
+      .col-lg-4.col-md-8.col-12.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema5/5.png', alt='Imagen decorativa')
+    
+    .row.justify-content-center.align-items-center 
+      .col-lg-4.col-md-8.col-12.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema5/6.png', alt='Imagen decorativa')
+      .col-lg-8.col-12.mb-3
+        .tarjeta.p-1(style="background-color: #EEFAFF")
+          .row.justify-content-center
+            .col-lg-10.col-12
+              ul.lista-ul
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Condiciones atmosféricas: incluye información sobre factores climáticos como la dirección y velocidad del viento, lluvia, temperatura, presión atmosférica y humedad.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Procedimiento para la medición de la velocidad del viento: describe cómo se midió la velocidad del viento.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Naturaleza/estado del terreno: ofrece información sobre el terreno entre la fuente y el receptor, incluyendo detalles sobre superficies, geometría, barreras y otros factores que influyen en los resultados.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Resultados numéricos y comparación con la normatividad aplicada: presenta los resultados de las mediciones y los compara con las normas aplicadas.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Descripción de los tiempos de medición: detalla los tiempos de medición, intervalos y procedimientos de referencia.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Variabilidad de las fuentes: describe la variabilidad de las fuentes de ruido.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Descripción de las fuentes de sonido: proporciona información cualitativa sobre las fuentes de ruido.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Reporte de memoria de cálculo: incluye detalles sobre la incertidumbre, ajustes y otros cálculos realizados.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Conclusiones y recomendaciones: resume las conclusiones del informe y ofrece recomendaciones basadas en los resultados.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Croquis detallado: muestra gráficamente la posición de las fuentes de sonido, objetos relevantes y puntos de observación y medición.
+                li 
+                  i.fas.fa-check(style="color: #63C500")
+                  | #[b] Copia de los certificados de calibración electrónica de los equipos: se adjuntan copias de los certificados de calibración de los instrumentos utilizados.
+    .row
+      p(data-aos="fade-left") Esta estructura proporciona una base sólida para la presentación de informes de monitoreo de ruido que sean completos y precisos.
+      
 
 </template>
 
