8c6506c0d70630741ae39f80eebb0a1708b1c6c4
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-11-15 14:25:47 -0500

    Update vídeo

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,7 +12,7 @@
 
     figure(data-aos="fade-right")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/L2gpCDe5rGw?si=2bePbJJtuOl2lrH3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 </template>
 
